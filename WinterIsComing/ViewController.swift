51a2f2f6716b742b044111b965ff8e766f70ca72
S@GoTa <tataidas1993@gmail.com>
2019-03-26 17:08:01 +0530

    added different targets

--- WinterIsComing/ViewController.swift
+++ WinterIsComing/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewChrome: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.imageViewChrome.image = UIImage(named: infoForKey("a")!)
+        
+        #if PRODUCTION
+        print("Production")
+        #elseif DEVELOPMENT
+        print("Development")
+        #else
+        print("Test")
+        #endif
+        
+    }
+    
+    func infoForKey(_ key: String) -> String? {
+        return (Bundle.main.infoDictionary?[key] as? String)?
+            .replacingOccurrences(of: "\\", with: "")
     }
 
 
