2064a1ebecbb9582b1daec3cf0532d18ab06ef7d
S@GoTa <tataidas1993@gmail.com>
2019-03-27 12:46:50 +0530

    s

--- WinterIsComing/ViewController.swift
+++ WinterIsComing/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.imageViewChrome.image = UIImage(named: infoForKey("a")!)
         
-        
+        //
         
     }
     
