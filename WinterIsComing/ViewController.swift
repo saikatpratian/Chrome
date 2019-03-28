aebf720f0a247b4ab35abe6b48a6e92866212656
S@GoTa <tataidas1993@gmail.com>
2019-03-28 18:04:25 +0530

    dsfsgv

--- WinterIsComing/ViewController.swift
+++ WinterIsComing/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageViewChrome: UIImageView!
     var s = ""
-    
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
